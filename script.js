094ae201a6ff841db1bb9c3f7d4073d229ad8aa9
Logan Woolf <loganwoolf@gmail.com>
2021-09-10 23:38:40 -0600

    create gameboard, players, display controller

--- script.js
+++ script.js
@@ -1,2 +1,47 @@
 "use strict"
 
+
+const gameBoard = (() => {
+	const marks = ['X', 'O', null, null, 'X', null, 'O', 'O', 'X']
+	// const marks = Array(9).fill(null)
+	return {
+		marks,
+
+	}
+})()
+
+const displayController = (() => {
+	const jsTarget = document.querySelector('.js-target')
+	const board = document.createElement('div')
+	board.classList.add('board')
+	jsTarget.appendChild(board)
+
+	gameBoard.marks.forEach( (element, index) => {
+		console.log(element);
+		const square = document.createElement('div')
+		square.classList.add('square')
+		square.setAttribute('data-square-id', index)
+		board.appendChild(square)
+		
+		if (element) {
+			const mark = document.createElement('p')
+			mark.classList.add('mark')
+			mark.textContent = element
+			square.appendChild(mark)
+		}
+	})
+
+})()
+
+const flowController = (() => {
+
+})()
+
+const playerFactory = (name, mark) => {
+	//add functions here
+	return {name, mark}
+}
+
+const playerOne = playerFactory('Player 1', 'X')
+const playerTwo = playerFactory('Player 2', 'O')
+
