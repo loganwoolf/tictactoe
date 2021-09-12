5715c91581029ebde65c1b70c08113baf0e8e6e7
Logan Woolf <loganwoolf@gmail.com>
2021-09-12 01:54:51 -0600

    add marks to board on click

--- script.js
+++ script.js
@@ -1,47 +1,75 @@
 "use strict"
 
 
-const gameBoard = (() => {
-	const marks = ['X', 'O', null, null, 'X', null, 'O', 'O', 'X']
-	// const marks = Array(9).fill(null)
-	return {
-		marks,
+
+const playerFactory = (name, mark) => {
+
+	const markBoard = (event) => {
+		let square = event.target.dataset.squareId
+		if (!gameBoard.marks[square]) {
+			gameBoard.marks[square] = mark
+		}
+		//change player here
+		displayController.updateGameBoard()
 
 	}
+
+	return {name, mark, markBoard}
+}
+
+const playerOne = playerFactory('Player 1', '❌')
+const playerTwo = playerFactory('Player 2', '⭕️')
+const players = [playerOne, playerTwo]
+const currentPlayer = players[0]
+
+
+
+const gameBoard = (() => {
+	// const marks = ['❌', '⭕️', null, null, '❌', null, '⭕️', '⭕️', null]
+	const marks = Array(9).fill(null)
+	return {
+		marks,
+		
+	}
 })()
+
+
 
 const displayController = (() => {
 	const jsTarget = document.querySelector('.js-target')
 	const board = document.createElement('div')
+
 	board.classList.add('board')
 	jsTarget.appendChild(board)
+	board.addEventListener('click', currentPlayer.markBoard)
 
 	gameBoard.marks.forEach( (element, index) => {
-		console.log(element);
 		const square = document.createElement('div')
 		square.classList.add('square')
 		square.setAttribute('data-square-id', index)
 		board.appendChild(square)
 		
-		if (element) {
-			const mark = document.createElement('p')
-			mark.classList.add('mark')
-			mark.textContent = element
-			square.appendChild(mark)
-		}
+		const mark = document.createElement('p')
+		mark.classList.add('mark')
+		mark.textContent = element
+		square.appendChild(mark)
 	})
 
+	const updateGameBoard = () => {
+		document.querySelectorAll('.square').forEach( item => {
+			if (gameBoard.marks[item.dataset.squareId]) {
+				item.children[0].textContent = gameBoard.marks[item.dataset.squareId]
+			}
+			
+		})
+	}
+
+	return {
+		updateGameBoard,
+
+	}
+
 })()
 
-const flowController = (() => {
 
-})()
-
-const playerFactory = (name, mark) => {
-	//add functions here
-	return {name, mark}
-}
-
-const playerOne = playerFactory('Player 1', 'X')
-const playerTwo = playerFactory('Player 2', 'O')
 
